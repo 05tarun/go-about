6a4168c274bfa4746fd184fa13e81266f81ee3fc
05tarun <tarunb@hotmail.com>
2016-04-10 22:03:35 -0400

    Added files via upload

---
+++ DIApplication/DIApplication/Bootstrapper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DIApplication.Implementation;
+using DIApplication.InterceptionBehaviors;
+using DIApplication.Interfaces;
+using Microsoft.Practices.Unity;
+using Microsoft.Practices.Unity.Configuration;
+using Microsoft.Practices.Unity.InterceptionExtension;
+
+namespace DIApplication
+{
+    public class Bootstrapper
+    {
+        IUnityContainer _Container;
+
+        public IUnityContainer Register()
+        {
+            var container = new UnityContainer();
+#if UseConfiguration
+            container.LoadConfiguration();
+#else
+            container.AddNewExtension<Interception>();
+            container.RegisterType<IObjectBase, ObjectBase>(
+                new Interceptor<InterfaceInterceptor>(),
+                new InterceptionBehavior<LoggingInterceptionBehavior>(),
+                new InterceptionBehavior<CachingInterceptionBehavior>(),
+                new InjectionConstructor(typeof(string), typeof(string), typeof(string)));
+#endif
+            _Container = container;
+
+            return container;
+        }
+    }
+}
---
+++ DIApplication/DIApplication/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DIApplication.Interfaces;
+using Microsoft.Practices.Unity;
+using Microsoft.Practices.Unity.InterceptionExtension;
+
+namespace DIApplication
+{
+    class Program
+    {
+        static IUnityContainer _Container;
+        static void Main(string[] args)
+        {
+            var bootstrapper = new Bootstrapper();
+            _Container = bootstrapper.Register();
+
+            var obj = _Container.Resolve<IObjectType>(new ResolverOverride[] {new ParameterOverride("name", "Name1"), new ParameterOverride("code", "Code1"), new ParameterOverride("description", "Descripion of Code1") });
+            var code = obj.GetCode();
+            var desc = obj.GetDescription();
+
+            Console.WriteLine("Received: ");
+            Console.WriteLine(code);
+            Console.WriteLine(desc);
+            Console.ReadKey();
+        }
+    }
+}
